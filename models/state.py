27e4a50a9ba2ddf385d5a76411fc766045f14d42
rania3103 <raniaabassi33@gmail.com>
2023-12-12 10:19:39 +0100

    add else

--- models/state.py
+++ models/state.py
@@ -12,15 +12,15 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
-
-    @property
-    def cities(self):
-        """returns the list of City instances
-        with state_id equals to the current State.id"""
-        from models import storage
-        l = []
-        res = storage.all("City")
-        for v in res.values():
-            if v["state_id"] == self.id:
-                l.append(v)
-        return l
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances
+            with state_id equals to the current State.id"""
+            from models import storage
+            l = []
+            res = storage.all("City")
+            for v in res.values():
+                if v["state_id"] == self.id:
+                    l.append(v)
+            return l
