12f5f3eac523683973b44e7c4e29f73ee09ddc3e
rania3103 <raniaabassi33@gmail.com>
2023-12-12 10:35:34 +0100

    fix table name

--- models/amenity.py
+++ models/amenity.py
@@ -23,4 +23,4 @@ class Amenity(BaseModel, Base):
     """this is the class amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="places_amenities_table")
+    place_amenities = relationship("Place", secondary="place_amenity")
