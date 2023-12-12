87db14465df0ff8e45da6538a67511b8448e5852
rania3103 <raniaabassi33@gmail.com>
2023-12-12 10:12:52 +0100

    add close method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,8 @@ class FileStorage:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in self.__objects.keys():
                 del (self.__objects[key])
+
+    def close(self):
+        """call reload() method for deserializing
+        the JSON file to objects"""
+        self.reload()
