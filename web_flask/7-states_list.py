9fbbdebbd5518b1ccb3177a2ceb401ae00a7ce6b
rania3103 <raniaabassi33@gmail.com>
2023-12-16 09:56:06 +0100

    update docstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application and displays states"""
+"""
+a script that starts a Flask web application and displays states
+"""
 from models import storage
 from flask import Flask, render_template
 
@@ -8,8 +10,10 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def show():
-    """display a HTML page: (inside the tag BODY) with a
-    list of states sorted by the name"""
+    """
+    display a HTML page: (inside the tag BODY) with a
+    list of states sorted by the name
+    """
     from models.state import State
     states = list(storage.all(State).values())
     return render_template("7-states_list.html", states=states)
