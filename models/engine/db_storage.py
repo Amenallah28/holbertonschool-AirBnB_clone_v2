84ab4426a8cf490c1f0e593ce80baa90ec97c746
rania3103 <raniaabassi33@gmail.com>
2023-12-18 16:48:11 +0100

    try to fix error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,7 +68,7 @@ class DBStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-        if isinstance(self.__engine, create_engine):
+        if isinstance(self.__engine, create_engine()):
             make_s = sessionmaker(bind=self.__engine, expire_on_commit=False)
             scop_session = scoped_session(make_s)
             self.__session = scop_session()
