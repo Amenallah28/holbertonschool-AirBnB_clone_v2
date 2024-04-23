d7d301ed6d5f4ce4d9d4d58495ef62c4ea3fd354
Amenallah28 <amenallahmrabet9@gmail.com>
2024-04-23 20:59:33 +0100

    fixing a typo

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,9 +36,9 @@ def display_n(n):
     """display n only if n in an int"""
     return "{:d} is a number".format(n)
 
-@app.route('/number_template/<ont:n>',strict_slashes=False)
+@app.route('/number_template/<int:n>',strict_slashes=False)
 def display_template(n):
-    return render_template('5-number.html',n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
