d58b0b000435c573bf15eaecf65b16a8d5726617
Amenallah28 <amenallahmrabet9@gmail.com>
2024-04-23 20:07:31 +0100

    display “n is a number” only if n is an integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,47 +1,39 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
+
 from flask import Flask
-from markupsafe import escape
-app = Flask(__name__)
+app=Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def show():
+@app.route('/',strict_slashes=False)
+def display():
     """display Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def show2():
-    """display HBNB"""
-    return "HBNB"
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    """display the string HBNB"""
+    return"HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def show3(text):
-    """display “C ” followed by the value
-    of the text variable (replace
-    underscore _ symbols with a space )"""
-    text = text.replace("_", " ")
-    return "C {}".format(escape(text))
+@app.route('/c/<text>',strict_slashes=False)
+def displayC(text):
+    """ display “C ” followed by the value of the text variable 
+    (replace underscore _ symbols with a space ) """
+    return "C " + text.replace('_', ' ')
 
 
-@app.route("/python/<text>", strict_slashes=False)
-@app.route("/python", strict_slashes=False)
-def show4(text="is cool"):
-    """display “Python ”, followed by the value of
-    the text variable (replace
-    underscore _ symbols with a space """
-    text = text.replace("_", " ")
-    return "Python {}".format(escape(text))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def displayPython(text='is cool'):
+    """display python + a text"""
+    return "Python " + text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def show5(n):
-    """display “n is a number”
-    only if n is an integer"""
-    return "{} is a number".format(escape(n))
+@app.route('/number/<n:int>')
+def display_n(n):
+    return "{:d} is a number".format(n)
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
